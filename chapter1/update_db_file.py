ba96c7900ad35792f5e23ec7ca939e630975b631
Qiyuan Pu <pqy7172@gmail.com>
2017-02-14 16:16:23 +0800

    add test result

--- chapter1/update_db_file.py
+++ chapter1/update_db_file.py
@@ -1,3 +1,17 @@
+"""
+The test result:
+pqy@sda1:~/.../chapter1$ python update_db_file.py 
+pqy@sda1:~/.../chapter1$ python dump_db_file.py 
+tom =>
+  {'job': None, 'name': 'Tom Tom', 'pay': 0, 'age': 50}
+sue =>
+  {'job': 'hdw', 'name': 'Sue Jones', 'pay': 44000.0, 'age': 45}
+bob =>
+  {'job': 'dev', 'name': 'Bob Smith', 'pay': 30000, 'age': 42}
+Sue Jones
+"""
+
+
 from make_db_file import loadDbase, storeDbase
 db = loadDbase()
 db['sue']['pay'] *= 1.10
