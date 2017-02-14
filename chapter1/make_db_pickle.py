a4586ba791c33af5011df6695383154255b9b40a
Qiyuan Pu <pqy7172@gmail.com>
2017-02-14 16:30:12 +0800

    test for make_db_pickle.py

--- chapter1/make_db_pickle.py
+++ chapter1/make_db_pickle.py
@@ -1,3 +1,19 @@
+"""
+Test result:
+
+pqy@sda1:~/.../chapter1$ python make_db_pickle.py 
+pqy@sda1:~/.../chapter1$ python dump_db_pickle.py 
+sue =>
+  {'job': 'hdw', 'pay': 40000, 'age': 45, 'name': 'Sue Jones'}
+bob =>
+  {'job': 'dev', 'pay': 30000, 'age': 42, 'name': 'Bob Smith'}
+tom =>
+  {'job': None, 'pay': 0, 'age': 50, 'name': 'Tom'}
+Sue Jones
+"""
+
+
+
 from initdata import db
 import pickle
 dbfile = open('people-pickle', 'wb')
